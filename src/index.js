2f544bd76b3c94627661000210b213edfc5702f0
gar7777 <gar@tut.by>
2020-10-05 12:35:50 +0300

    test: tipical array problrms BlinovG

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if (array == undefined) {
+    return 0;
+  }
+	else if (array.length == 0) {
+    return 0;
+  }
+
+  let minValue = array[0];
+    for (i = 0; i < array.length; i++){
+      if (minValue > array[i]) {
+        minValue = array[i];
+      }
+    }
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if (array == undefined) {
+    return 0;
+  }
+	else if (array.length == 0) {
+    return 0;
+  }
+
+  let maxValue = array[0];
+    for (i = 0; i < array.length; i++){
+      if (maxValue < array[i]){
+        maxValue = array[i];
+      }
+    }
+  
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  if (array == undefined) {
+    return 0;
+  }
+	else if (array.length == 0) {
+    return 0;
+  }
+
+  let summ = 0;
+  for (i = 0; i < array.length; i++) {
+    summ = summ + array[i];
+  }
+  return summ / array.length;
 }
